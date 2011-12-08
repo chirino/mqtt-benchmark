eb90e012c8548283a0f0f94306cc4bc300472b3c
Hiram Chirino <hiram@hiramchirino.com>
2011-12-08 17:08:07 -0500

    Better benchmark running scripts.

---
+++ scripts/benchmark-apollo.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# This shell script automates running the stomp-benchmark [1] against the
+# Apache Apollo project [2].
+#
+# [1]: http://github.com/chirino/stomp-benchmark
+# [2]: http://activemq.apache.org/apollo
+# [3]: http://aws.amazon.com/amazon-linux-ami/
+#
+
+APOLLO_VERSION=1.0-beta6
+APOLLO_DOWNLOAD='https://repository.apache.org/content/repositories/orgapacheactivemq-299/org/apache/activemq/apache-apollo/1.0-beta6/apache-apollo-1.0-beta6-unix-distro.tar.gz'
+BENCHMARK_HOME=~/benchmark
+. benchmark-setup.sh
+
+#
+# Install the apollo distro
+#
+if [ ! -d "${BENCHMARK_HOME}/apache-apollo-${APOLLO_VERSION}" ]; then
+  cd ${BENCHMARK_HOME}
+  wget "$APOLLO_DOWNLOAD"
+  tar -zxvf apache-apollo-*.tar.gz
+  rm -rf apache-apollo-*.tar.gz
+fi
+if [ ! -d "${BENCHMARK_HOME}/apollo-${APOLLO_VERSION}" ]; then
+  cd "${BENCHMARK_HOME}"
+  "${BENCHMARK_HOME}/apache-apollo-${APOLLO_VERSION}/bin/apollo" create "apollo-${APOLLO_VERSION}"
+fi
+
+rm -rf ${BENCHMARK_HOME}/apollo-${APOLLO_VERSION}/data/*
+rm -rf ${BENCHMARK_HOME}/apollo-${APOLLO_VERSION}/tmp/*
+rm -rf ${BENCHMARK_HOME}/apollo-${APOLLO_VERSION}/log/*
+
+kilall -9 java 2> /dev/null
+kilall -9 apollo 2> /dev/null
+
+#
+# Start the broker
+#
+rm "${BENCHMARK_HOME}/apollo-${APOLLO_VERSION}/log/console.log" 2> /dev/null
+"${BENCHMARK_HOME}/apollo-${APOLLO_VERSION}/bin/apollo-broker" run 2>&1 > "${BENCHMARK_HOME}/apollo-${APOLLO_VERSION}/log/console.log" &
+APOLLO_PID=$!
+echo "Started Apollo with PID: ${APOLLO_PID}"
+sleep 5
+cat ${BENCHMARK_HOME}/apollo-${APOLLO_VERSION}/log/console.log
+
+#
+# Run the benchmark
+#
+cd ${BENCHMARK_HOME}/stomp-benchmark
+sbt run --login admin --passcode password reports/apache-apollo-${APOLLO_VERSION}.json
+
+# Kill the apollo
+kill -9 ${APOLLO_PID}
---
+++ scripts/benchmark-setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+mkdir -p ${BENCHMARK_HOME}
+
+#
+# Install SBT
+#
+if [ ! -f "${BENCHMARK_HOME}/bin/sbt" ] ; then
+  mkdir ~/.ivy2 2> /dev/null
+  mkdir ${BENCHMARK_HOME}/bin 2> /dev/null
+  cd ${BENCHMARK_HOME}/bin
+  wget http://simple-build-tool.googlecode.com/files/sbt-launch-0.7.4.jar
+  echo '#!/bin/sh
+  java -server -Xmx4G -XX:MaxPermSize=500m -jar ${BENCHMARK_HOME}/bin/sbt-launch-0.7.4.jar "$*"
+  ' > ${BENCHMARK_HOME}/bin/sbt
+  chmod a+x "${BENCHMARK_HOME}/bin/sbt"
+fi
+
+#
+# Install git so we can get and run the stomp-benchmark
+#
+which git > /dev/null
+if [ $? -ne 0 ] ; then
+  cd ${BENCHMARK_HOME}
+  sudo yum install -y git
+fi 
+
+if [ ! -d "${BENCHMARK_HOME}/stomp-benchmark" ] ; then
+  cd ${BENCHMARK_HOME}
+  git clone git://github.com/chirino/stomp-benchmark.git
+  cd stomp-benchmark
+  sbt update
+fi
+
